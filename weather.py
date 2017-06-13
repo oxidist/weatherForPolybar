ef6a516c32c3e742c396789afa4940799f1f7fa1
OfficialOxide <best.ali2002@gmail.com>
2017-06-13 16:25:23 +0530

    Initial commit

---
+++ weather.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+import pyowm
+
+api_key = 'd2e1cb17172b8247f4eac5f53b458e3c'
+owm = pyowm.OWM(api_key)
+
+# How to enter the argument city:
+#   (your city),(your two letter country code)
+#   for example, the argument for London would be:
+#   'London,uk'
+
+# How to enter the argument unit:
+#   Enter a string, 'C' or 'F' for Celsius unit or Fahrenheit unit respectively.
+
+def get_weather_of_city(city, unit):
+    weather_in_city = owm.weather_at_place(city)
+    weather = weather_in_city.get_weather()
+    temp_in_kelvin = weather.get_temperature()
+    status = weather.get_status()
+    print(status)
+
+    if unit is "C":
+        celsius = pyowm.utils.temputils.kelvin_to_celsius(temp_in_kelvin['temp'])
+        print(celsius)
+        if status is "Clouds":
+            # return " %s, %d°C" % (status, celsius)
+            print("%s, %d °C" % (status, celsius))
+        if status is "Clear":
+            return " %s, %d°C" % (status, celsius)
+        if status is "Rains":
+            return " %s, %d°C" % (status, celsius)
+
+    if unit is "F":
+        fahrenheit = pyowm.utils.temputils.kelvin_to_fahrenheit(temp_in_kelvin['temp'])
+        if status is "Clouds":
+            return " %s, %d°F" % (status, fahrenheit)
+        if status is "Clear":
+            return " %s, %d°F" % (status, fahrenheit)
+        if status is "Rains":
+            return " %s, %d°F" % (status, fahrenheit)
+
+#print(get_weather_of_city("Bhopal,IN", "C"))
+get_weather_of_city('Bhopal,', "C")
