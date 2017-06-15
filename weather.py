77e90d9fe997c98cb17ca69fa582275158933f6d
OfficialOxide <best.ali2002@gmail.com>
2017-06-15 13:30:15 +0530

    Major bug fixes

--- weather.py
+++ weather.py
@@ -1,40 +1,56 @@
 #!/usr/bin/python
 import pyowm
 
-api_key = 'your-API-key-here'
+api_key = 'your-API-key-here' # get api key from OWM after signing up
 owm = pyowm.OWM(api_key)
 
-# How to enter the argument city:
-#   (your city),(your two letter country code)
-#   for example, the argument for London would be:
-#   'London,uk'
+class WeatherGetter(object):
+    # How to enter the argument city:
+    #   (your city),(your two letter country code)
+    #   for example, the argument for London would be:
+    #   'London,uk'
 
-# How to enter the argument unit:
-#   Enter a string, 'C' or 'F' for Celsius unit or Fahrenheit unit respectively.
+    # How to enter the argument unit:
+    #   Enter a string, 'C' or 'F' for Celsius unit or Fahrenheit unit respectively.
+    def __init__(self, city, unit):
+        self.city = city
+        self.unit = unit
 
-def get_weather_of_city(city, unit):
-    weather_in_city = owm.weather_at_place(city)
-    weather = weather_in_city.get_weather()
-    temp_in_kelvin = weather.get_temperature()
-    status = weather.get_status()
+    def get_weather_of_city(self, ):
+        weather_in_city = owm.weather_at_place(self.city)
+        weather = weather_in_city.get_weather() # returns a Weather object (?)
+        return weather
 
-    if unit is 'C':
-        celsius = pyowm.utils.temputils.kelvin_to_celsius(temp_in_kelvin['temp'])
+    def get_temp_of_city(self, weather):
+        temp_in_kelvin = weather.get_temperature() # Returns a dict
+        return temp_in_kelvin
 
-        if status == 'Clouds':
-            return ' {},{}°C'.format(status, celsius)
-        elif status == 'Clear':
-            return ' {},{}°C'.format(status, celsius)
-        elif status == 'Rains':
-            return ' {},{}°C'.format(status, celsius)
+    def get_status_of_city(self, weather):
+        status = weather.get_status() # for eg. sunny, clouds, etc.
+        return status
+    def output_the_weather(self, temp_in_kelvin, status):
+        if self.unit is 'C':
+            celsius = pyowm.utils.temputils.kelvin_to_celsius(temp_in_kelvin['temp'])
+            if status == 'Clouds':
+                return ' {},{}°C'.format(status, celsius)
+            elif status == 'Clear':
+                return ' {},{}°C'.format(status, celsius)
+            elif status == 'Rains':
+                return ' {},{}°C'.format(status, celsius)
+        elif self.unit is "F":
+            fahrenheit = pyowm.utils.temputils.kelvin_to_fahrenheit(temp_in_kelvin['temp'])
+            if status == 'Clouds':
+                return ' {},{}°F'.format(status, fahrenheit)
+            elif status == 'Clear':
+                return ' {},{}°F'.format(status, fahrenheit)
+            elif status == 'Rains':
+                return ' {},{}°F'.format(status, fahrenheit)
 
-    if unit is "F":
-        fahrenheit = pyowm.utils.temputils.kelvin_to_fahrenheit(temp_in_kelvin['temp'])
-        if status == 'Clouds':
-            return ' {},{}°F'.format(status, fahrenheit)
-        elif status == 'Clear':
-            return ' {},{}°F'.format(status, fahrenheit)
-        elif status == 'Rains':
-            return ' {},{}°F'.format(status, fahrenheit)
+reporter = WeatherGetter('YOUR CITY, YOUR 2-LETTER COUNTRY CODE','YOUR UNIT HERE')
 
-print(get_weather_of_city("YOUR CITY HERE, YOUR TWO LETTER COUNTRY CODE HERE", "YOUR UNIT HERE (C OR F)"))
+weather = reporter.get_weather_of_city()
+
+status = reporter.get_status_of_city(weather)
+temp = reporter.get_temp_of_city(weather)
+
+print (reporter.output_the_weather(temp, status))
